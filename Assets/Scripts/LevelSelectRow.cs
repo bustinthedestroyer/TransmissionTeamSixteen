ac46e01e5f3d0e8378e18dfc37817ddd08520b2d
bustinthedestroyer <brooksjustind@gmail.com>
2018-01-28 16:15:21 -0600

    Sunday

---
+++ Assets/Scripts/LevelSelectRow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelectRow : MonoBehaviour {
+
+	public Text LevelNumber, LevelName;
+	public Button SelectButton;
+	public Transmission transmission;
+
+	public int LevelId;
+	public string LevelTitle;
+	public string LevelTransmission;
+	
+
+	void Start(){
+		Refresh();
+	}
+
+	public void Refresh()
+	{
+		LevelNumber.text = LevelId.ToString() + ".";
+		LevelName.text = LevelTitle;
+
+		Debug.Log(PlayerPrefs.GetInt("HighestLevel",0));
+		int HighestLevel = PlayerPrefs.GetInt("HighestLevel",0);
+
+		if(HighestLevel+1 >= LevelId){
+			SelectButton.interactable  = true;
+		}else{
+			SelectButton.interactable  = false;
+		}
+	}
+
+	public void StartLevel(){
+		transmission.StartGame(LevelId, LevelTitle, LevelTransmission);
+	}
+}
---
+++ Assets/Scripts/Levels.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Levels{
+
+
+	public static Level[] Stages = new Level[]{
+		new Level(){
+			LevelId = 1,
+			LevelTitle = "Level 1",
+			LevelTransmission = "I am you from the future"			
+		},
+		new Level(){
+			LevelId = 2,
+			LevelTitle = "Level 2",
+			LevelTransmission = "With an important message"			
+		},
+		new Level(){
+			LevelId = 3,
+			LevelTitle = "Level 3",
+			LevelTransmission = "About the past"			
+		}
+	};
+}
+public class Level {
+	public int LevelId;
+	public string LevelTitle;
+	public string LevelTransmission;
+}
